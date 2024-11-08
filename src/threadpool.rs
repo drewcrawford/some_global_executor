c1a6b0b4766182de28c4eb9aa7e3bf4a9f7e4d16
Drew Crawford <drew@sealedabstract.com>
2024-11-08 13:31:28 -0600

    Initial commit

---
+++ src/threadpool.rs
@@ -0,0 +1,29 @@
+use std::thread::JoinHandle;
+
+pub struct Threadpool {
+    vec: Vec<JoinHandle<()>>,
+}
+
+impl Threadpool {
+    pub fn new(name: &str, size: usize) -> Threadpool {
+        let mut vec = Vec::with_capacity(size);
+        for t in 0..size {
+            let handle = std::thread::Builder::new()
+                .name(format!("some_global_executor {}-{}", name, t))
+                .spawn(|| {
+                    println!("Hello from a thread!");
+                })
+                .unwrap();
+            vec.push(handle);
+        }
+        Threadpool { vec }
+    }
+}
+
+#[cfg(test)] mod tests {
+    use crate::threadpool::Threadpool;
+
+    #[test] fn new() {
+        let t = Threadpool::new("test", 4);
+    }
+}
